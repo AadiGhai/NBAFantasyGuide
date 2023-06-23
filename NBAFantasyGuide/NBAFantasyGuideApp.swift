f1d0eb122f60e6a8c351c07d109beaa944129cd8
Aadi Ghai <aaditheghai@gmail.com>
2023-06-23 16:03:27 -0700

    Initial Commit

---
+++ NBAFantasyGuide/NBAFantasyGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBAFantasyGuideApp.swift
+//  NBAFantasyGuide
+//
+//  Created by Aadi Ghai on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct NBAFantasyGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
